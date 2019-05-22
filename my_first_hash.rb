b01dc7002ded86d3d503c271c8691c7269a7f99d
Justin <sumbhodi1618@gmail.com>
2019-05-22 06:57:41 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -12,9 +12,8 @@ def retrieval
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-
-  # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-
+                 
+shipping_manifest["oil paintings"]
 end
 
 def adding
@@ -24,13 +23,13 @@ def adding
                   "oil paintings" => 3
                  }
   # add 2 muskets to the shipping_manifest hash below
-
+adding["muskets"] => "2"
 
 
   # add 4 gun powder to the shipping_manifest hash below
-
+adding["gun powder"] => "4"
 
 
   # return the shipping_manifest hash below
-
+adding
 end
